19d70e2625936eb80ce4a3fd824a7f35bb7c44d9
Mario Nikolov <mario_606@abv.bg>
2026-03-08 19:20:44 +0200

    Initial commit

---
+++ src/main/java/com/logiclab/documentcontrolsystem/DocumentControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logiclab.documentcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/logiclab/documentcontrolsystem/DocumentControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logiclab.documentcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
