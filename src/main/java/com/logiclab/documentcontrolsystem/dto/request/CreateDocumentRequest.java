6f048ebde01788e6a9bc0fb5821998d0e70cf65a
Mario Nikolov <mario_606@abv.bg>
2026-03-23 17:59:01 +0200

    Created CommentService

--- src/main/java/com/logiclab/documentcontrolsystem/dto/request/CreateDocumentRequest.java
+++ src/main/java/com/logiclab/documentcontrolsystem/dto/request/CreateDocumentRequest.java
@@ -1,15 +1,9 @@
 package com.logiclab.documentcontrolsystem.dto.request;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateDocumentRequest {
     private String title;
     private String description;
--- src/main/java/com/logiclab/documentcontrolsystem/dto/request/CreateUserRequest.java
+++ src/main/java/com/logiclab/documentcontrolsystem/dto/request/CreateUserRequest.java
@@ -6,9 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CreateUserRequest {
     private String username;
     private String email;
