2b6f0b0104b54ce35ac7c2114cef64d62bead609
Mario Nikolov <mario_606@abv.bg>
2026-03-23 18:46:57 +0200

    Improved AuditlLog entity

---
+++ src/main/java/com/logiclab/documentcontrolsystem/domain/AuditAction.java
@@ -0,0 +1,9 @@
+package com.logiclab.documentcontrolsystem.domain;
+
+public enum AuditAction {
+    CREATE,
+    UPDATE,
+    DELETE,
+    APPROVE,
+    REJECT
+}
---
+++ src/main/java/com/logiclab/documentcontrolsystem/domain/AuditEntityType.java
@@ -0,0 +1,9 @@
+package com.logiclab.documentcontrolsystem.domain;
+
+public enum AuditEntityType {
+    DOCUMENT,
+    DOCUMENT_VERSION,
+    COMMENT,
+    USER,
+    VERSION_REVIEW
+}
