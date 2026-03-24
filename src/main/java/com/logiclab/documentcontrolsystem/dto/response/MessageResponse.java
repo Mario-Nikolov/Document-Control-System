baed4cb17cb7d687935399f11f9d48275c334e03
Kircho <kiriltanov@gmail.com>
2026-03-24 21:00:40 +0200

    created dto-response

---
+++ src/main/java/com/logiclab/documentcontrolsystem/dto/response/MessageResponse.java
@@ -0,0 +1,10 @@
+package com.logiclab.documentcontrolsystem.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+public class MessageResponse {
+    private String message;
+}
