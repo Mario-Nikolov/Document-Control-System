076571eee5c3151b25b3aeb1d73b512cfd756b1e
Mario Nikolov <mario_606@abv.bg>
2026-03-25 22:19:23 +0200

    Created AuthController

---
+++ src/main/java/com/logiclab/documentcontrolsystem/web/AuthController.java
@@ -0,0 +1,21 @@
+package com.logiclab.documentcontrolsystem.web;
+
+import com.logiclab.documentcontrolsystem.dto.request.LoginRequest;
+import com.logiclab.documentcontrolsystem.dto.response.AuthResponse;
+import com.logiclab.documentcontrolsystem.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
